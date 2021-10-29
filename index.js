e8c251e4b30051a40e26b3289fff734816d8fd50
Suhag Al Amin <developersuhag@gmail.com>
2021-10-29 15:31:20 +0600

    conntect

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qow90.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+console.log(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("conntected");
+  } finally {
+    // await client.close()
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
